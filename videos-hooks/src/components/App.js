dc65d08c4ef195a82cde8cde2cce051bdababf11
Paul Fell <swellpf@gmail.com>
2021-10-02 20:26:49 -0400

    created custom hook in useVideos.js file

--- videos-hooks/src/components/App.js
+++ videos-hooks/src/components/App.js
@@ -5,36 +5,21 @@ import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
 
 const App = () => {
-const [videos, setVideos] = useState([]);
 const [selectedVideo, setSelectedVideo] = useState(null);
 
-useEffect(() => {
-    onTermSubmit('youtube');
 
-}, []);
+// setSelectedVideo(response.data.items[0])
 
-    const onTermSubmit = async term => {
-        const response = await youtube.get('/search', {
-            params: {
-                q: {term}
-            }
-        });
-        
-        setVideos(response.data.items);
-        setSelectedVideo(response.data.items[0])
+    
 
-    };
 
-    const onVideoSelect = (video) => {
-        setSelectedVideo(video);
-    };
     return (
         <div className="ui container">
             <SearchBar onFormSubmit={onTermSubmit}/>
             <div className="ui grid">
                 <div className="ui row">
                     <div className="eleven wide column"><VideoDetail video={selectedVideo}/></div>
-                    <div className="five wide column"><VideoList onVideoSelect={onVideoSelect} videos={videos}/></div>
+                    <div className="five wide column"><VideoList onVideoSelect={setSelectedVideo} videos={videos}/></div>
                 </div>
             </div>
         </div>
